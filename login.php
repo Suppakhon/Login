caff86eb06102392de4c0b1d6ae249aab37310c1
Nueng <suppakhon11@gmail.com>
2019-05-21 11:55:51 +0700

    commit 1

---
+++ login.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-874">
+        <link rel="icon" href="face.png">
+    <title>
+        SPsoft Digital
+    </title>
+    <link rel="stylesheet" type="text/css" href ="style.css">
+    <body>
+        <div class="loginbox">
+         <img src="avatar-icon.png" class="avatar">
+         <h1>เข้าสู่ระบบ</h1>
+         <form>
+             <p>Username</p>
+             <input type="text" name="" placeholder="Enter Username">
+             <p>Password</p>
+             <input type="password" name=""placeholder="Enter Password">
+             <input type="submit"name=""value="Login">
+
+        
+             <a href="https://ces.wu.ac.th/registrar/login.asp">ลืมรหัสผ่าน ?</a><br>
+             <a href="https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp">สมัครสมาชิกใหม่ </a>
+
+         </form>
+     
+        </div>
+    
+        <footer>
+            <div class="text">
+                    <h1>Smart Reporting Systems</h1>
+                    <br><br><br><br><br><br><br><br><br>
+                    <br><br><br><br><br><br><br><br><br>
+                    <br><br><br><br><br><br><br><br><br>
+                    <p>Copyright &copy; 2019 | SPsoft Digital </p>
+            </div>
+            
+        </footer>
+    </body>
+</head>
+
+</html>
